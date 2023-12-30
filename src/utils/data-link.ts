6985e3c858f8e9f1290e7cd416c0d097d1453b2d
jhean <brizadaojhean@gmail.com>
2023-12-30 00:08:35 -0300

    :sparkles: feat: dados para sub-rotas de desafios e docs

---
+++ src/utils/data-link.ts
@@ -0,0 +1,50 @@
+export type TypeDataLinks = {
+  href: string
+  content: string
+}
+
+export const dataLinksDesafios: TypeDataLinks[] = [
+  {
+    href: '/desafios/person',
+    content: 'Pessoas'
+  },
+  {
+    href: '/desafios/planet',
+    content: 'Planetas'
+  },
+  {
+    href: '/desafios/starships',
+    content: 'Nave Estelares'
+  },
+  {
+    href: '/desafios/vehicle',
+    content: 'Veículos'
+  },
+  {
+    href: '/desafios/species',
+    content: 'Espécies'
+  }
+]
+
+export const dataLinksDocs: TypeDataLinks[] = [
+  {
+    href: '/docs/person',
+    content: 'Pessoas'
+  },
+  {
+    href: '/docs/planet',
+    content: 'Planeta'
+  },
+  {
+    href: '/docs/starships',
+    content: 'Nave Estelares'
+  },
+  {
+    href: '/docs/vehicle',
+    content: 'Veículo'
+  },
+  {
+    href: '/docs/species',
+    content: 'Espécies'
+  }
+]
