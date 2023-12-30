8db64210841e4a855108b793d6075bbc391f139b
jhean <brizadaojhean@gmail.com>
2023-12-30 00:40:28 -0300

    🚧 feat: progresso a ui da rota desafios

--- src/app/desafios/page.tsx
+++ src/app/desafios/page.tsx
@@ -1,5 +1,11 @@
+import SectionDesafioPerson from '@/components/segmentos/desafios/components/section-desafio-person'
+
 function PageDesafio() {
-  return <main>fui redecionado para desafio</main>
+  return (
+    <main className="mt-8 flex flex-col gap-8">
+      <SectionDesafioPerson />
+    </main>
+  )
 }
 
 export default PageDesafio
