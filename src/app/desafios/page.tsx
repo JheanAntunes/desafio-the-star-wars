b5df27aa4728355651651ad67a8cf0d3afefd953
jhean <brizadaojhean@gmail.com>
2024-01-02 19:49:12 -0300

    :truck: chore: import do component SectionDesafioPlanet na UI da rota desafios.

--- src/app/desafios/page.tsx
+++ src/app/desafios/page.tsx
@@ -1,9 +1,11 @@
 import SectionDesafioPerson from '@/components/segmentos/desafios/components/section/section-person/section-desafio-person'
+import SectionDesafioPlanet from '@/components/segmentos/desafios/components/section/section-planet'
 
 function PageDesafio() {
   return (
     <main className="mt-8 flex flex-col gap-8">
       <SectionDesafioPerson />
+      <SectionDesafioPlanet />
     </main>
   )
 }
