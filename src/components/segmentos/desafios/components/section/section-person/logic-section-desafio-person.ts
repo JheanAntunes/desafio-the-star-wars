98f0dd99fd982147eb6c7b3188954dd8285d0b1a
jhean <brizadaojhean@gmail.com>
2024-01-02 19:23:19 -0300

    refactor: atualizando o import da função modifying_data_improve_UI -> modifying_data_Person_improve_UI

--- src/components/segmentos/desafios/components/section/section-person/logic-section-desafio-person.ts
+++ src/components/segmentos/desafios/components/section/section-person/logic-section-desafio-person.ts
@@ -1,11 +1,11 @@
 import { TypeFetchPerson } from '@/types/Typesfetch'
 import { BASE_URL_API_PEOPLE, fetchGet } from '@/utils/fetch'
-import { modifying_data_improve_UI } from '@/utils/modiying_data_improve_ui'
+import { modifying_data_Person_improve_UI } from '@/utils/modiying_data_improve_ui'
 
 async function LogicSectionDesafioPerson() {
   const { results } = await fetchGet<TypeFetchPerson>(BASE_URL_API_PEOPLE)
 
-  const data_Improved_UI = modifying_data_improve_UI(results)
+  const data_Improved_UI = modifying_data_Person_improve_UI(results)
 
   return {
     data_Improved_UI
