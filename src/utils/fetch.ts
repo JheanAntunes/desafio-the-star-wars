1ff75899f8a90a7e7aba6b674b6fdc516d65280a
jhean <brizadaojhean@gmail.com>
2024-01-02 19:24:59 -0300

    :sparkles: feat: adicionando uma nova BASE_URL da planet

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,5 @@
 export const BASE_URL_API_PEOPLE = 'https://swapi.dev/api/people/?format=json'
+export const BASE_URL_API_Planets = 'https://swapi.dev/api/planets/?format=json'
 
 export const fetchGet = async <TData>(url: string, options?: RequestInit) => {
   const response = await fetch(url, options)
