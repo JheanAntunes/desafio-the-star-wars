a2e3c0e41229c6b03fa8d9d9ed852eb5c7c73adc
jhean <brizadaojhean@gmail.com>
2024-01-03 18:40:03 -0300

    :recycle:	refactor: melhorando a tipagem com generics no typeFetch

--- src/components/segmentos/desafios/components/section-planet/logic-section-planet.ts
+++ src/components/segmentos/desafios/components/section-planet/logic-section-planet.ts
@@ -1,9 +1,10 @@
-import { TypeFetchPlanet } from '@/types/Typesfetch'
+import { TypeFetch, TypePlanet } from '@/types/Typesfetch'
 import { BASE_URL_API_Planets, fetchGet } from '@/utils/fetch'
 import { modifying_data_Planet_improve_UI } from '@/utils/modiying_data_improve_ui'
 
 export const LogicSectionPlanet = async () => {
-  const { results } = await fetchGet<TypeFetchPlanet>(BASE_URL_API_Planets)
+  const { results } =
+    await fetchGet<TypeFetch<TypePlanet>>(BASE_URL_API_Planets)
 
   const data_improved_UI = modifying_data_Planet_improve_UI(results)
 
