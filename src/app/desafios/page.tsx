5c495da28aa4dd92bfdae07d1b483585affd30a8
jhean <brizadaojhean@gmail.com>
2024-01-02 23:38:39 -0300

    :sparkles: feat: import do component SectionDesafioFilms na rota de desafios

--- src/app/desafios/page.tsx
+++ src/app/desafios/page.tsx
@@ -1,3 +1,4 @@
+import SectionDesafioFilms from '@/components/segmentos/desafios/components/section-films'
 import SectionDesafioPerson from '@/components/segmentos/desafios/components/section-person/section-desafio-person'
 import SectionDesafioPlanet from '@/components/segmentos/desafios/components/section-planet'
 
@@ -6,6 +7,7 @@ function PageDesafio() {
     <main className="mt-8 flex flex-col gap-8">
       <SectionDesafioPerson />
       <SectionDesafioPlanet />
+      <SectionDesafioFilms />
     </main>
   )
 }
