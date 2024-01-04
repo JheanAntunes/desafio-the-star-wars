2e736a3838b51c7c4d44c6a3fd4379d0abe33c64
jhean <brizadaojhean@gmail.com>
2024-01-03 21:07:26 -0300

    :sparkles: feat: nova BASE_URL, BASE_URL_API_VEHICLES

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,6 +4,8 @@ export const BASE_URL_API_Planets = 'https://swapi.dev/api/planets/?format=json'
 export const BASE_URL_API_Films = 'https://swapi.dev/api/films/?format=json'
 export const BASE_URL_API_STARSHIPS =
   'https://swapi.dev/api/starships/?format=json'
+export const BASE_URL_API_VEHICLES =
+  'https://swapi.dev/api/vehicles/?format=json'
 
 export const fetchGet = async <TData>(url: string, options?: RequestInit) => {
   const response = await fetch(url, options)
