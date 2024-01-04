70a04172a0b4f88e40a6aade28d29e5e1efd4f1b
jhean <brizadaojhean@gmail.com>
2024-01-03 23:50:08 -0300

    :sparkles: feat: A lógica do Component de SubList que recebe as mesma props do component para fazer o break do array e se estiver break mostrar a qtd que não foi mostrado.

---
+++ src/components/segmentos/desafios/components/section-species/logic-sub-list.tsx
@@ -0,0 +1,42 @@
+import { TypographyListItem, TypographySmall } from '@/components/ui/typography'
+import { TypeSubListProps } from './sub-list'
+
+const LogicSubList = ({
+  dataCaracteristicas,
+  break_by_index
+}: TypeSubListProps) => {
+  const isBreak = (index: number) => {
+    if (
+      break_by_index &&
+      break_by_index <= dataCaracteristicas.length &&
+      break_by_index < index
+    ) {
+      return true
+    }
+    return false
+  }
+
+  const IsBreakShowQuantityMissing = () => {
+    if (
+      break_by_index &&
+      break_by_index < dataCaracteristicas.length &&
+      dataCaracteristicas.length - (break_by_index + 1) !== 0
+    ) {
+      return (
+        <TypographyListItem>
+          <TypographySmall className="text-zinc-900">
+            tem mais {dataCaracteristicas.length - (break_by_index + 1)}.
+          </TypographySmall>
+        </TypographyListItem>
+      )
+    }
+    return null
+  }
+
+  return {
+    isBreak,
+    IsBreakShowQuantityMissing
+  }
+}
+
+export default LogicSubList
