970b997514a594c502327aa6950e68ab1a909ae5
jhean <brizadaojhean@gmail.com>
2024-01-03 23:47:07 -0300

    :sparkles: feat: Component de sub-list que recebe como props uma array de string e possuí break opcional

---
+++ src/components/segmentos/desafios/components/section-species/sub-list.tsx
@@ -0,0 +1,36 @@
+import {
+  TypographyList,
+  TypographyListItem,
+  TypographySmall
+} from '@/components/ui/typography'
+import LogicSubList from './logic-sub-list'
+
+export type TypeSubListProps = {
+  dataCaracteristicas: string[]
+  break_by_index?: number
+}
+const SubList = ({ dataCaracteristicas, break_by_index }: TypeSubListProps) => {
+  const { isBreak, IsBreakShowQuantityMissing } = LogicSubList({
+    dataCaracteristicas,
+    break_by_index
+  })
+
+  return (
+    <TypographyList>
+      {dataCaracteristicas.map((caracteristica, index) => {
+        if (isBreak(index)) return null
+
+        return (
+          <TypographyListItem key={caracteristica}>
+            <TypographySmall className="text-zinc-900">
+              {caracteristica}
+            </TypographySmall>
+          </TypographyListItem>
+        )
+      })}
+      {IsBreakShowQuantityMissing()}
+    </TypographyList>
+  )
+}
+
+export default SubList
