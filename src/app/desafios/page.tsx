1c9863dbc904b115099a7563053a5b4584ff67fc
jhean <brizadaojhean@gmail.com>
2024-01-03 21:12:30 -0300

    :sparkles: feat: Import Component SectionDesafioVehicles na rota de desafios

--- src/app/desafios/page.tsx
+++ src/app/desafios/page.tsx
@@ -2,6 +2,7 @@ import SectionDesafioFilms from '@/components/segmentos/desafios/components/sect
 import SectionDesafioPerson from '@/components/segmentos/desafios/components/section-person/section-desafio-person'
 import SectionDesafioPlanet from '@/components/segmentos/desafios/components/section-planet'
 import SectionDesafioStarships from '@/components/segmentos/desafios/components/section-starships'
+import SectionDesafioVehicles from '@/components/segmentos/desafios/components/section-vehicle'
 
 function PageDesafio() {
   return (
@@ -10,6 +11,7 @@ function PageDesafio() {
       <SectionDesafioPlanet />
       <SectionDesafioFilms />
       <SectionDesafioStarships />
+      <SectionDesafioVehicles />
     </main>
   )
 }
