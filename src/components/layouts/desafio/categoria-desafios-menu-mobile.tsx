7355ff6d9f10e01191c7fc575b8d226560d3cdfb
jhean <brizadaojhean@gmail.com>
2024-01-04 21:22:42 -0300

    :sparkles: feat: Menu mobile com component Dropdown para menu de categorias

---
+++ src/components/layouts/desafio/categoria-desafios-menu-mobile.tsx
@@ -0,0 +1,41 @@
+'use client'
+import useActiveLinkPathname from '@/components/hooks/useActiveLinkPathname'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
+import { TypeDataLinks } from '@/utils/data-link'
+import { NavigationIcon } from 'lucide-react'
+import Link from 'next/link'
+
+type TypeCategoriaActiveLinkProps = {
+  dataLinks: TypeDataLinks[]
+}
+
+export default function CategoriaDesafiosDropdownMenuItems({
+  dataLinks
+}: TypeCategoriaActiveLinkProps) {
+  const { activeLink } = useActiveLinkPathname()
+
+  return (
+    <>
+      {dataLinks.map(({ href, content }) => (
+        <DropdownMenuItem
+          key={href + content}
+          className={cn(
+            '',
+            activeLink(href)
+              ? 'hoverLink after:w-full hover:after:w-0'
+              : 'hoverLink'
+          )}
+        >
+          <Link
+            href={href}
+            className={cn('flex items-center gap-1 font-sans capitalize')}
+          >
+            {content}
+            <NavigationIcon width={14} height={14} />
+          </Link>
+        </DropdownMenuItem>
+      ))}
+    </>
+  )
+}
