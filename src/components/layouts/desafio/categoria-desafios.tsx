d894a1fdcc09e7e8cb8c3c260343bb1b46462ca0
jhean <brizadaojhean@gmail.com>
2024-01-04 21:24:17 -0300

    :sparkles: feat: menu-mobile de categorias de desafios adicionado.

--- src/components/layouts/desafio/categoria-desafios.tsx
+++ src/components/layouts/desafio/categoria-desafios.tsx
@@ -1,18 +1,26 @@
 import EspadaLuz from '@/components/assets/espada-luz.png'
+import { Button } from '@/components/ui/button'
 import CategoriaActiveLink from '@/components/ui/categoria-active-link-by-pathname'
 import {
   CategoriaContainer,
   CategoriaContainerListLinks,
   CategoriaTitle
 } from '@/components/ui/categoria-links'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 import { dataLinksDesafios } from '@/utils/data-link'
+import { ArrowDown } from 'lucide-react'
 import Image from 'next/image'
+import CategoriaDesafiosDropdownMenuItems from './categoria-desafios-menu-mobile'
 
 function CategoriaDesafios() {
   return (
     <>
       <CategoriaContainer>
-        <div className="flex gap-1">
+        <div className="flex file:gap-1">
           <CategoriaTitle>Categorias de Desafios</CategoriaTitle>
           <div className="relative h-10 w-10 overflow-hidden">
             <Image
@@ -23,6 +31,20 @@ function CategoriaDesafios() {
               className="object-contain"
             />
           </div>
+        </div>
+        <div className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="gap-1">
+                Menu de Categorias Desafios <ArrowDown height={14} width={14} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="flex flex-col gap-5">
+              <CategoriaDesafiosDropdownMenuItems
+                dataLinks={dataLinksDesafios}
+              />
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         <CategoriaContainerListLinks>
           <CategoriaActiveLink dataLinks={dataLinksDesafios} />
