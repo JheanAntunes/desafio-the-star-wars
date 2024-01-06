290f2aa51e6d7240c3823c06ca517b623ba13de1
jhean <brizadaojhean@gmail.com>
2024-01-05 23:45:28 -0300

    :sparkles: feat: Component PagePersonDesafio: Rota: /desafios/planets Component que representa UI da rota. props: searchParams (optional) Fetch: Os fetchs estão isolados em components para não bloquear a interface toda. Reutilização de skeletons em suspense: SkeletonCards, PlanetPagination

---
+++ src/app/desafios/planets/page.tsx
@@ -0,0 +1,34 @@
+import PlanetCards from '@/components/segmentos/desafios/planets/components/planet-cards'
+import PlanetPagination from '@/components/segmentos/desafios/planets/components/planet-pagination'
+import { SkeletonCards } from '@/components/ui/skeletons/skeleton-cards'
+import SkeletonPagination from '@/components/ui/skeletons/skeleton-pagination'
+import { TypographyH2, TypographyP } from '@/components/ui/typography'
+import { Suspense } from 'react'
+
+type TypeParamsSearch = {
+  page?: string
+}
+function PagePersonDesafio({
+  searchParams
+}: {
+  searchParams: TypeParamsSearch
+}) {
+  const { page } = searchParams
+
+  return (
+    <main className="container my-8 flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <TypographyH2>Desafio de Planetas</TypographyH2>
+        <TypographyP>Clique em algum desafio abaixo para começar.</TypographyP>
+      </div>
+      <Suspense fallback={<SkeletonCards />}>
+        <PlanetCards page={page} />
+      </Suspense>
+      <Suspense fallback={<SkeletonPagination />}>
+        <PlanetPagination page={page} />
+      </Suspense>
+    </main>
+  )
+}
+
+export default PagePersonDesafio
