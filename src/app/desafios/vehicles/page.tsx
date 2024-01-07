cb9b3c084b2b7776306b9d9212327107df9f58a1
jhean <brizadaojhean@gmail.com>
2024-01-06 21:10:26 -0300

    :sparkles: feat: UI da rota Vehicles de desafios - Component PageVehiclesDesafio

---
+++ src/app/desafios/vehicles/page.tsx
@@ -0,0 +1,34 @@
+import VehiclesCards from '@/components/segmentos/desafios/vehicles/components/vehicles-cards'
+import VehiclesPagination from '@/components/segmentos/desafios/vehicles/components/vehicles-pagination'
+import { SkeletonCards } from '@/components/ui/skeletons/skeleton-cards'
+import SkeletonPagination from '@/components/ui/skeletons/skeleton-pagination'
+import { TypographyH2, TypographyP } from '@/components/ui/typography'
+import { Suspense } from 'react'
+
+type TypeParamsSearch = {
+  page?: string
+}
+function PageVehiclesDesafio({
+  searchParams
+}: {
+  searchParams: TypeParamsSearch
+}) {
+  const { page } = searchParams
+
+  return (
+    <main className="container my-8 flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <TypographyH2>Desafio de Veículos</TypographyH2>
+        <TypographyP>Clique em algum desafio abaixo para começar.</TypographyP>
+      </div>
+      <Suspense fallback={<SkeletonCards />}>
+        <VehiclesCards page={page} />
+      </Suspense>
+      <Suspense fallback={<SkeletonPagination />}>
+        <VehiclesPagination page={page} />
+      </Suspense>
+    </main>
+  )
+}
+
+export default PageVehiclesDesafio
