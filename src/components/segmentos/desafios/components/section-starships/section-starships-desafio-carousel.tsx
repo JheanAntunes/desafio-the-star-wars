0b8780326be0d395db3f72ac94447bafb723c869
jhean <brizadaojhean@gmail.com>
2024-01-07 19:48:08 -0300

    :recycle: refactor: utilizando o component SectionDesafioCarousel e adicionando os sliders e movendo o fetch para o component que utiliza os dados.

--- src/components/segmentos/desafios/components/section-starships/section-starships-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-starships/section-starships-desafio-carousel.tsx
@@ -1,40 +1,21 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from '@/components/ui/carousel'
-import { TypeModifyingStarships } from '@/utils/modiying_data_improve_ui'
+import { CarouselItem } from '@/components/ui/carousel'
 import StarshipsCard from '../../starships/components/starships-card'
+import SectionDesafioCarousel from '../section-desafio-carousel'
+import LogicSectionDesafioCarouselStarships from './logic-section-desafio-carousel-starships'
 
-type TypeSectionDesafioPersonCarouselProps = {
-  data_Improved_UI: TypeModifyingStarships
-}
-
-function SectionStarshipsDesafioCarousel({
-  data_Improved_UI
-}: TypeSectionDesafioPersonCarouselProps) {
+async function SectionStarshipsDesafioCarousel() {
+  const { data_improved_UI } = await LogicSectionDesafioCarouselStarships()
   return (
-    <Carousel
-      opts={{
-        align: 'start'
-      }}
-      className="w-full max-w-xs md:max-w-3xl lg:max-w-5xl"
-    >
-      <CarouselContent className="-ml-8">
-        {data_Improved_UI.map((dataStarship) => (
-          <CarouselItem
-            key={dataStarship.created + dataStarship.MGLT}
-            className="pl-8 md:basis-1/2 lg:basis-1/3"
-          >
-            <StarshipsCard {...dataStarship} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <SectionDesafioCarousel>
+      {data_improved_UI.map((dataStarship) => (
+        <CarouselItem
+          key={dataStarship.created + dataStarship.MGLT}
+          className="pl-8 md:basis-1/2 lg:basis-1/3"
+        >
+          <StarshipsCard {...dataStarship} />
+        </CarouselItem>
+      ))}
+    </SectionDesafioCarousel>
   )
 }
 
