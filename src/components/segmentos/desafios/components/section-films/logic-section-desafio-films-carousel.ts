2370974239e9f95769747dc5ea500e79eb0fee28
jhean <brizadaojhean@gmail.com>
2024-01-07 20:14:45 -0300

    :recycle: refactor: SectionDesafioFilms: movendo a lógica para o component SectionFilmsDesafioCarousel

--- src/components/segmentos/desafios/components/section-films/logic-section-desafio-films-carousel.ts
+++ src/components/segmentos/desafios/components/section-films/logic-section-desafio-films-carousel.ts
@@ -2,7 +2,7 @@ import { TypeFetch, TypeFilms } from '@/types/Typesfetch'
 import { BASE_URL_API_Films, fetchGet } from '@/utils/fetch'
 import { modifying_data_Films_improve_UI } from '@/utils/modiying_data_improve_ui'
 
-async function LogicSectionDesafioFilms() {
+async function LogicSectionDesafioFilmsCarousel() {
   const { results } = await fetchGet<TypeFetch<TypeFilms>>(BASE_URL_API_Films)
 
   const data_Improved_UI = modifying_data_Films_improve_UI(results)
@@ -12,4 +12,4 @@ async function LogicSectionDesafioFilms() {
   }
 }
 
-export default LogicSectionDesafioFilms
+export default LogicSectionDesafioFilmsCarousel
--- src/components/segmentos/desafios/components/section-films/section-films-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-films/section-films-desafio-carousel.tsx
@@ -1,40 +1,21 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from '@/components/ui/carousel'
-import { TypeModifyingFilms } from '@/utils/modiying_data_improve_ui'
+import { CarouselItem } from '@/components/ui/carousel'
 import FilmCard from '../../films/components/film-card'
+import SectionDesafioCarousel from '../section-desafio-carousel'
+import LogicSectionDesafioFilmsCarousel from './logic-section-desafio-films-carousel'
 
-type TypeSectionDesafioPersonCarouselProps = {
-  data_Improved_UI: TypeModifyingFilms
-}
-
-function SectionFilmsDesafioCarousel({
-  data_Improved_UI
-}: TypeSectionDesafioPersonCarouselProps) {
+async function SectionFilmsDesafioCarousel() {
+  const { data_Improved_UI } = await LogicSectionDesafioFilmsCarousel()
   return (
-    <Carousel
-      opts={{
-        align: 'start'
-      }}
-      className="w-full max-w-xs md:max-w-3xl lg:max-w-5xl"
-    >
-      <CarouselContent className="-ml-8">
-        {data_Improved_UI.map((dataFilm) => (
-          <CarouselItem
-            key={dataFilm.created + dataFilm.characters}
-            className="pl-8 md:basis-1/2 lg:basis-1/3"
-          >
-            <FilmCard {...dataFilm} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <SectionDesafioCarousel>
+      {data_Improved_UI.map((dataFilm) => (
+        <CarouselItem
+          key={dataFilm.created + dataFilm.characters}
+          className="pl-8 md:basis-1/2 lg:basis-1/3"
+        >
+          <FilmCard {...dataFilm} />
+        </CarouselItem>
+      ))}
+    </SectionDesafioCarousel>
   )
 }
 
