85aba63aaeb975cbc3f3a7dfea45f2ee504e0016
jhean <brizadaojhean@gmail.com>
2024-01-07 20:23:00 -0300

    :recycle: refactor: SectionDesafioPlanet: movendo a lógica para o component SectionPlanetDesafioCarousel que mapeia os dados e cria os sliders

--- src/components/segmentos/desafios/components/section-planet/index.tsx
+++ src/components/segmentos/desafios/components/section-planet/index.tsx
@@ -1,9 +1,7 @@
 import { TypographyH2, TypographyP } from '@/components/ui/typography'
-import { LogicSectionPlanet } from './logic-section-planet'
 import SectionPlanetDesafioCarousel from './section-planet-desafio-carousel'
 
-async function SectionDesafioPlanet() {
-  const { data_improved_UI } = await LogicSectionPlanet()
+function SectionDesafioPlanet() {
   return (
     <section className="container flex flex-col gap-5">
       <div>
@@ -11,7 +9,7 @@ async function SectionDesafioPlanet() {
         <TypographyP>Clique no desafio abaixo para começar.</TypographyP>
       </div>
       <div className="flex items-center justify-center">
-        <SectionPlanetDesafioCarousel data_Improved_UI={data_improved_UI} />
+        <SectionPlanetDesafioCarousel />
       </div>
     </section>
   )
--- src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
@@ -1,40 +1,21 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from '@/components/ui/carousel'
-import { TypeModifyingPlanet } from '@/utils/modiying_data_improve_ui'
+import { CarouselItem } from '@/components/ui/carousel'
 import PlanetCard from '../../planets/components/planet-card'
+import SectionDesafioCarousel from '../section-desafio-carousel'
+import { LogicSectionDesafioPlanetCarousel } from './logic-section-desafio-planet-carousel'
 
-type TypeSectionDesafioPersonCarouselProps = {
-  data_Improved_UI: TypeModifyingPlanet
-}
-
-function SectionPlanetDesafioCarousel({
-  data_Improved_UI
-}: TypeSectionDesafioPersonCarouselProps) {
+async function SectionPlanetDesafioCarousel() {
+  const { data_improved_UI } = await LogicSectionDesafioPlanetCarousel()
   return (
-    <Carousel
-      opts={{
-        align: 'start'
-      }}
-      className="w-full max-w-xs md:max-w-3xl lg:max-w-5xl"
-    >
-      <CarouselContent className="-ml-8">
-        {data_Improved_UI.map((dataPlanet) => (
-          <CarouselItem
-            key={dataPlanet.created + dataPlanet.climate}
-            className="pl-8 md:basis-1/2 lg:basis-1/3"
-          >
-            <PlanetCard {...dataPlanet} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <SectionDesafioCarousel>
+      {data_improved_UI.map((dataPlanet) => (
+        <CarouselItem
+          key={dataPlanet.created + dataPlanet.climate}
+          className="pl-8 md:basis-1/2 lg:basis-1/3"
+        >
+          <PlanetCard {...dataPlanet} />
+        </CarouselItem>
+      ))}
+    </SectionDesafioCarousel>
   )
 }
 
