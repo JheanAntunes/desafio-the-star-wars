5467e21e2040e7f4d8f1fd40f56540bd699bd8bb
jhean <brizadaojhean@gmail.com>
2024-01-06 23:06:04 -0300

    :recycle: refactor: SectionPlanetDesafioCarousel: removendo os components, Porque agora tem o Component PlanetCard que recebe os dados e mostra a mesma coisa.

--- src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
@@ -1,5 +1,3 @@
-import EspadaLuz from '@/components/assets/espada-luz.png'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   Carousel,
   CarouselContent,
@@ -7,12 +5,8 @@ import {
   CarouselNext,
   CarouselPrevious
 } from '@/components/ui/carousel'
-import { TypographyList } from '@/components/ui/typography'
-import { formatedNumber } from '@/utils/formatedNumber'
 import { TypeModifyingPlanet } from '@/utils/modiying_data_improve_ui'
-import Image from 'next/image'
-import Link from 'next/link'
-import ShorthandListItem from '../shorthand-list-item'
+import PlanetCard from '../../planets/components/planet-card'
 
 type TypeSectionDesafioPersonCarouselProps = {
   data_Improved_UI: TypeModifyingPlanet
@@ -29,74 +23,14 @@ function SectionPlanetDesafioCarousel({
       className="w-full max-w-xs md:max-w-3xl lg:max-w-5xl"
     >
       <CarouselContent className="-ml-8">
-        {data_Improved_UI.map(
-          ({
-            population,
-            climate,
-            diameter,
-            gravity,
-            orbital_period,
-            rotation_period,
-            terrain,
-            created
-          }) => (
-            <CarouselItem
-              key={created}
-              className="pl-8 md:basis-1/2 lg:basis-1/3"
-            >
-              <Link href="#" className="group">
-                <Card className="bg-transparent transition-colors group-hover:border-blue-500">
-                  <CardHeader className="pb-0">
-                    <div className="flex items-center gap-1">
-                      <CardTitle className="font-sans font-normal transition-colors group-hover:text-blue-500">
-                        Desafio
-                      </CardTitle>
-                      <Image
-                        src={EspadaLuz}
-                        alt=""
-                        height={32}
-                        width={32}
-                        className="object-contain"
-                      />
-                    </div>
-                  </CardHeader>
-                  <CardContent>
-                    <TypographyList className="group-hover:decoration-blue-500">
-                      <ShorthandListItem
-                        title="População:"
-                        caracteristica={formatedNumber(population)}
-                      />
-                      <ShorthandListItem
-                        title="Clima:"
-                        caracteristica={climate}
-                      />
-                      <ShorthandListItem
-                        title="Diâmetro:"
-                        caracteristica={formatedNumber(diameter)}
-                      />
-                      <ShorthandListItem
-                        title="Gravidade:"
-                        caracteristica={formatedNumber(gravity)}
-                      />
-                      <ShorthandListItem
-                        title="Periodo orbital:"
-                        caracteristica={`${formatedNumber(orbital_period)}`}
-                      />
-                      <ShorthandListItem
-                        title="Periodo de Rotação:"
-                        caracteristica={`${rotation_period}`}
-                      />
-                      <ShorthandListItem
-                        title="Terreno:"
-                        caracteristica={terrain}
-                      />
-                    </TypographyList>
-                  </CardContent>
-                </Card>
-              </Link>
-            </CarouselItem>
-          )
-        )}
+        {data_Improved_UI.map((dataPlanet) => (
+          <CarouselItem
+            key={dataPlanet.created + dataPlanet.climate}
+            className="pl-8 md:basis-1/2 lg:basis-1/3"
+          >
+            <PlanetCard {...dataPlanet} />
+          </CarouselItem>
+        ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
