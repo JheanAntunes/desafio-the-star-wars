99d12603dea865604c2e4d4700c3d140e76b198d
jhean <brizadaojhean@gmail.com>
2024-01-06 23:04:44 -0300

    :recycle: refactor: SectionFilmsDesafioCarousel: removendo os components, Porque agora tem o Component FilmCard que recebe os dados e mostra a mesma coisa.

--- src/components/segmentos/desafios/components/section-films/section-films-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-films/section-films-desafio-carousel.tsx
@@ -1,5 +1,3 @@
-import EspadaLuz from '@/components/assets/espada-luz.png'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   Carousel,
   CarouselContent,
@@ -7,15 +5,8 @@ import {
   CarouselNext,
   CarouselPrevious
 } from '@/components/ui/carousel'
-import {
-  TypographyList,
-  TypographyListItem,
-  TypographySmall
-} from '@/components/ui/typography'
 import { TypeModifyingFilms } from '@/utils/modiying_data_improve_ui'
-import Image from 'next/image'
-import Link from 'next/link'
-import ShorthandListItem from '../shorthand-list-item'
+import FilmCard from '../../films/components/film-card'
 
 type TypeSectionDesafioPersonCarouselProps = {
   data_Improved_UI: TypeModifyingFilms
@@ -32,66 +23,12 @@ function SectionFilmsDesafioCarousel({
       className="w-full max-w-xs md:max-w-3xl lg:max-w-5xl"
     >
       <CarouselContent className="-ml-8">
-        {data_Improved_UI.map(({ producer, episode_id, director, created }) => (
+        {data_Improved_UI.map((dataFilm) => (
           <CarouselItem
-            key={created}
+            key={dataFilm.created + dataFilm.characters}
             className="pl-8 md:basis-1/2 lg:basis-1/3"
           >
-            <Link href="#" className="group">
-              <Card className="bg-transparent transition-colors group-hover:border-blue-500">
-                <CardHeader className="pb-0">
-                  <div className="flex items-center gap-1">
-                    <CardTitle className="font-sans font-normal transition-colors group-hover:text-blue-500">
-                      Desafio
-                    </CardTitle>
-                    <Image
-                      src={EspadaLuz}
-                      alt=""
-                      height={32}
-                      width={32}
-                      className="object-contain"
-                    />
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <TypographyList className="group-hover:decoration-blue-500">
-                    <ShorthandListItem
-                      title="Diretor:"
-                      caracteristica={director}
-                    />
-                    <TypographyListItem className="text-zinc-700">
-                      <div className="flex flex-col">
-                        <TypographySmall className="text-zinc-900">
-                          produtor(es)
-                        </TypographySmall>
-                        <TypographyList className="my-2">
-                          {producer.split(' ').map((produtorNome, index) => {
-                            // Show two name producers in card
-                            if (index > 1) return null
-                            return (
-                              <TypographyListItem key={produtorNome}>
-                                <TypographySmall className="text-zinc-900">
-                                  {produtorNome}
-                                </TypographySmall>
-                              </TypographyListItem>
-                            )
-                          })}
-                          <TypographyListItem>
-                            <TypographySmall className="text-zinc-900">
-                              E mais {producer.split(' ').length} produtor(es)
-                            </TypographySmall>
-                          </TypographyListItem>
-                        </TypographyList>
-                      </div>
-                    </TypographyListItem>
-                    <ShorthandListItem
-                      title="O número do episódio deste filme:"
-                      caracteristica={episode_id.toString()}
-                    />
-                  </TypographyList>
-                </CardContent>
-              </Card>
-            </Link>
+            <FilmCard {...dataFilm} />
           </CarouselItem>
         ))}
       </CarouselContent>
