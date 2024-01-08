2c49ac9a748cc6cf4393af6736d2a8047ad21d88
jhean <brizadaojhean@gmail.com>
2024-01-07 21:16:40 -0300

    :recycle: refactor: Adicionando um link que redireciona para a rota dinamica de person com o id do dado como wrapper no Component PersonCard

--- src/components/segmentos/desafios/components/section-person/section-desafio-person-carousel.tsx
+++ src/components/segmentos/desafios/components/section-person/section-desafio-person-carousel.tsx
@@ -1,4 +1,5 @@
 import { CarouselItem } from '@/components/ui/carousel'
+import Link from 'next/link'
 import PersonCard from '../../person/components/PersonCard'
 import SectionDesafioCarousel from '../section-desafio-carousel'
 import LogicSectionDesafioPersonCarousel from './logic-section-desafio-person-carousel'
@@ -7,12 +8,14 @@ const SectionDesafioPersonCarousel = async () => {
   const { data_Improved_UI } = await LogicSectionDesafioPersonCarousel()
   return (
     <SectionDesafioCarousel>
-      {data_Improved_UI.map((dataPerson) => (
+      {data_Improved_UI.map((dataPerson, index) => (
         <CarouselItem
           key={dataPerson.created}
           className="pl-8 md:basis-1/2 lg:basis-1/3"
         >
-          <PersonCard dataFormatedImproveUi={dataPerson} />
+          <Link className="group" href={`/desafios/person/${index + 1}`}>
+            <PersonCard dataFormatedImproveUi={dataPerson} />
+          </Link>
         </CarouselItem>
       ))}
     </SectionDesafioCarousel>
