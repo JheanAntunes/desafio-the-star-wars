20e148fd81bc8f515ab74d5a68874378db09611a
jhean <brizadaojhean@gmail.com>
2024-01-07 21:13:18 -0300

    :recycle: refactor: removendo o link que estava como wrapper no Card do component PersonCard e adicionando como wrapper na chamada do component

--- src/components/segmentos/desafios/person/components/PersonCard.tsx
+++ src/components/segmentos/desafios/person/components/PersonCard.tsx
@@ -4,7 +4,6 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { TypographyList } from '@/components/ui/typography'
 import { TypePerson } from '@/types/Typesfetch'
 import Image from 'next/image'
-import Link from 'next/link'
 
 type TypePersonCardProps = {
   dataFormatedImproveUi: TypePerson
@@ -18,62 +17,55 @@ const PersonCard = ({ dataFormatedImproveUi }: TypePersonCardProps) => {
     gender,
     height,
     mass,
-    skin_color,
-    created
+    skin_color
   } = dataFormatedImproveUi
   return (
     <>
-      <Link
-        href="#"
-        className="group"
-        key={height + mass + skin_color + created}
-      >
-        <Card className="bg-transparent transition-colors group-hover:border-blue-500">
-          <CardHeader className="pb-0">
-            <div className="flex items-center gap-1">
-              <CardTitle className="font-sans font-normal transition-colors group-hover:text-blue-500">
-                Desafio
-              </CardTitle>
-              <Image
-                src={EspadaLuz}
-                alt=""
-                height={32}
-                width={32}
-                className="object-contain"
-              />
-            </div>
-          </CardHeader>
-          <CardContent>
-            <TypographyList className="group-hover:decoration-blue-500">
-              <ShorthandListItem
-                title="Ano de nascimento:"
-                caracteristica={birth_year}
-              />
-              <ShorthandListItem
-                title="A cor dos olhos:"
-                caracteristica={eye_color}
-              />
-              <ShorthandListItem
-                title="A cor do cabelo:"
-                caracteristica={hair_color}
-              />
-              <ShorthandListItem title="Gênero:" caracteristica={gender} />
-              <ShorthandListItem
-                title="A altura em centímetros:"
-                caracteristica={`${height}cm`}
-              />
-              <ShorthandListItem
-                title="A massa em quilogramas:"
-                caracteristica={`${mass}kg`}
-              />
-              <ShorthandListItem
-                title="A cor da pele:"
-                caracteristica={skin_color}
-              />
-            </TypographyList>
-          </CardContent>
-        </Card>
-      </Link>
+      <Card className="bg-transparent transition-colors group-hover:border-blue-500">
+        <CardHeader className="pb-0">
+          <div className="flex items-center gap-1">
+            <CardTitle className="font-sans font-normal transition-colors group-hover:text-blue-500">
+              Desafio
+            </CardTitle>
+            <Image
+              src={EspadaLuz}
+              alt=""
+              height={32}
+              width={32}
+              className="object-contain"
+            />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <TypographyList className="group-hover:decoration-blue-500">
+            <ShorthandListItem
+              title="Ano de nascimento:"
+              caracteristica={birth_year}
+            />
+            <ShorthandListItem
+              title="A cor dos olhos:"
+              caracteristica={eye_color}
+            />
+            <ShorthandListItem
+              title="A cor do cabelo:"
+              caracteristica={hair_color}
+            />
+            <ShorthandListItem title="Gênero:" caracteristica={gender} />
+            <ShorthandListItem
+              title="A altura em centímetros:"
+              caracteristica={`${height}cm`}
+            />
+            <ShorthandListItem
+              title="A massa em quilogramas:"
+              caracteristica={`${mass}kg`}
+            />
+            <ShorthandListItem
+              title="A cor da pele:"
+              caracteristica={skin_color}
+            />
+          </TypographyList>
+        </CardContent>
+      </Card>
     </>
   )
 }
