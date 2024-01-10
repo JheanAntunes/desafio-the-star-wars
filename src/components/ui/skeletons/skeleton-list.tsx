b6cc8c9962f666be0ce0659511dd185a3ce55657
jhean <brizadaojhean@gmail.com>
2024-01-10 19:27:11 -0300

    :nail_care: styles: Component SkeletonList: Limitando a largura para 320px

--- src/components/ui/skeletons/skeleton-list.tsx
+++ src/components/ui/skeletons/skeleton-list.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../skeleton'
 
 const SkeletonList = () => {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex max-w-80 flex-col gap-1">
       <Skeleton className="h-4 w-full pl-2" />
       <Skeleton className="h-4 w-full pl-2" />
       <Skeleton className="h-4 w-full pl-2" />
