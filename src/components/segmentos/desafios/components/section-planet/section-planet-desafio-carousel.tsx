55e7425f1324fc299e1bc850c7b9c7e04d821bbf
jhean <brizadaojhean@gmail.com>
2024-01-12 20:11:23 -0300

    :recycle: refactor: O link deve redirecionar o usuario para a rota dinamica de planets

--- src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
+++ src/components/segmentos/desafios/components/section-planet/section-planet-desafio-carousel.tsx
@@ -13,7 +13,7 @@ async function SectionPlanetDesafioCarousel() {
           key={dataPlanet.created + dataPlanet.climate}
           className="pl-8 md:basis-1/2 lg:basis-1/3"
         >
-          <Link className="group" href={`/desafios/person/${index}`}>
+          <Link className="group" href={`/desafios/planets/${index}`}>
             <PlanetCard {...dataPlanet} />
           </Link>
         </CarouselItem>
