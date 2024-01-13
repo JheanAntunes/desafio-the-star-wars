536215efdcd44aaa31135b8c3591bd073337b401
jhean <brizadaojhean@gmail.com>
2024-01-13 20:36:35 -0300

    :recycle: refactor: Component ChallengerCompleted: adicionando a props children para reutilizar esse component

--- src/components/segmentos/desafios/planets/[id]/components/challenger-completed.tsx
+++ src/components/segmentos/desafios/planets/[id]/components/challenger-completed.tsx
@@ -1,23 +1,21 @@
-import SkeletonCategorias from '@/components/ui/skeletons/skeleton-categorias'
 import { TypeFetch, TypeFetchCategory, TypePlanet } from '@/types/Typesfetch'
 import { BASE_URL_API, fetchGet } from '@/utils/fetch'
 import { formatedString } from '@/utils/formated-string'
-import { Suspense } from 'react'
 import UIChallengeCompleted from '../../../components/ui-challenge-completed'
-import DesafioPlanets from './desafio-planets'
 
 type TypeChallengerCompletedProps = {
   id: string
   page: string
   challengerCompleted?: string
   fetchCategory: TypeFetchCategory
-}
+} & React.PropsWithChildren
 
 const ChallengerCompleted = async ({
   id,
   page,
   fetchCategory,
-  challengerCompleted
+  challengerCompleted,
+  children
 }: TypeChallengerCompletedProps) => {
   const { results } = await fetchGet<TypeFetch<TypePlanet>>(
     `${BASE_URL_API}/${fetchCategory}/?page=${page}&format=json`
@@ -32,11 +30,7 @@ const ChallengerCompleted = async ({
     return <UIChallengeCompleted name={challengerCompleted} />
   }
 
-  return (
-    <Suspense fallback={<SkeletonCategorias />}>
-      <DesafioPlanets id={id} page={page} />
-    </Suspense>
-  )
+  return <>{children}</>
 }
 
 export default ChallengerCompleted
