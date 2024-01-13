f18a90ea54563cc25c61195161aaa731dd2e5709
jhean <brizadaojhean@gmail.com>
2024-01-12 21:49:50 -0300

    :recycle: refactor: Component DontShowComponentChallengeCompleted: utilizando o Component UIChallengeCompleted que vai substituir o formulario

--- src/components/segmentos/desafios/person/[id]/components/dont-show-component-challenge-completed.tsx
+++ src/components/segmentos/desafios/person/[id]/components/dont-show-component-challenge-completed.tsx
@@ -1,7 +1,6 @@
 'use client'
-import { Badge } from '@/components/ui/badge'
-import { TypographyH1 } from '@/components/ui/typography'
 import { useContextChallengePersonCompleted } from '@/context/context-challenge-person-completed'
+import UIChallengeCompleted from '../../../components/ui-challenge-completed'
 
 type TypeDontShowComponentChallengeCompleted = {
   secretCaracteristica: string
@@ -13,17 +12,7 @@ const DontShowComponentChallengeCompleted = ({
   const { challengeCompleted } = useContextChallengePersonCompleted()
 
   return challengeCompleted ? (
-    <div className="grid grid-cols-1 gap-1">
-      <Badge
-        className="w-fit animate-fade-down border-emerald-500 font-sans font-normal animate-delay-100 animate-duration-500 animate-once animate-ease-linear md:mx-auto"
-        variant="outline"
-      >
-        Desafio Concluído
-      </Badge>
-      <TypographyH1 className="animate-fade animate-delay-100 animate-duration-500 animate-once animate-ease-linear md:text-center lg:text-center">
-        {secretCaracteristica}
-      </TypographyH1>
-    </div>
+    <UIChallengeCompleted name={secretCaracteristica} />
   ) : (
     children
   )
