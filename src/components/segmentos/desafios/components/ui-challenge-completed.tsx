0eb7adfb5ac4875f98303217e2d2d3a7ac28ef4c
jhean <brizadaojhean@gmail.com>
2024-01-12 21:48:39 -0300

    :sparkles: feat: Component UIChallengeCompleted: Component que substitui o formulario quando o usuario acerta o desafio.

---
+++ src/components/segmentos/desafios/components/ui-challenge-completed.tsx
@@ -0,0 +1,23 @@
+import { Badge } from '@/components/ui/badge'
+import { TypographyH1 } from '@/components/ui/typography'
+
+type TypeUIChallengeCompleted = {
+  name: string
+}
+const UIChallengeCompleted = ({ name }: TypeUIChallengeCompleted) => {
+  return (
+    <div className="grid grid-cols-1 gap-1">
+      <Badge
+        className="w-fit animate-fade-down border-emerald-500 font-sans font-normal animate-delay-100 animate-duration-500 animate-once animate-ease-linear md:mx-auto"
+        variant="outline"
+      >
+        Desafio Concluído
+      </Badge>
+      <TypographyH1 className="animate-fade animate-delay-100 animate-duration-500 animate-once animate-ease-linear md:text-center lg:text-center">
+        {name}
+      </TypographyH1>
+    </div>
+  )
+}
+
+export default UIChallengeCompleted
