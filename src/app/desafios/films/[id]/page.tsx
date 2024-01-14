29abc781b940c9addd8a6726f6190880eab00a00
jhean <brizadaojhean@gmail.com>
2024-01-13 22:47:53 -0300

    :sparkles: feat: Component PageChallengeFilm: Representa a UI da página dinamica de desafios da categoria film

---
+++ src/app/desafios/films/[id]/page.tsx
@@ -0,0 +1,42 @@
+import ChallengeCompletedFilm from '@/components/segmentos/desafios/films/[id]/components/challenge-completed-film'
+import DesafioFilmDescription from '@/components/segmentos/desafios/films/[id]/components/desafio-film-description'
+import { Skeleton } from '@/components/ui/skeleton'
+import SkeletonCategorias from '@/components/ui/skeletons/skeleton-categorias'
+import { Suspense } from 'react'
+
+type TypeURLParams = {
+  id: string
+}
+
+type TypeURLSeachParams = {
+  page?: string
+  challengerCompleted?: string
+}
+
+type TypePageChallengeFilm = {
+  params: TypeURLParams
+  searchParams: TypeURLSeachParams
+}
+
+const PageChallengeFilm = ({ params, searchParams }: TypePageChallengeFilm) => {
+  const { id } = params
+  const { challengerCompleted, page } = searchParams
+  const currentPage = page ?? '1'
+
+  return (
+    <main className="container py-8">
+      <Suspense fallback={<Skeleton className="h-80 w-full" />}>
+        <ChallengeCompletedFilm
+          id={id}
+          page={currentPage}
+          challengerCompleted={challengerCompleted}
+        />
+      </Suspense>
+      <Suspense fallback={<SkeletonCategorias />}>
+        <DesafioFilmDescription id={id} page={currentPage} />
+      </Suspense>
+    </main>
+  )
+}
+
+export default PageChallengeFilm
