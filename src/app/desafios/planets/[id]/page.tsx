ccc006b3d49f414d07b4bc67fda2d03ed99f7c48
jhean <brizadaojhean@gmail.com>
2024-01-13 21:11:09 -0300

    :sparkles: feat: Component ChallengerCompletedPlanet: Utilizando o Component ChallengerCompletedPlanet que verificar se o desafio está completado, se estiver, renderiza a UI de desafio completado, se não, renderiza a UI de desafios

--- src/app/desafios/planets/[id]/page.tsx
+++ src/app/desafios/planets/[id]/page.tsx
@@ -1,6 +1,5 @@
 import SkeletonDesafioPersonDescription from '@/components/segmentos/desafios/person/[id]/components/skeletons/skeletonDesafioPersonDescription'
-import ChallengerCompleted from '@/components/segmentos/desafios/planets/[id]/components/challenger-completed'
-import DesafioPlanets from '@/components/segmentos/desafios/planets/[id]/components/desafio-planets'
+import ChallengerCompletedPlanet from '@/components/segmentos/desafios/planets/[id]/components/challenger-completed-planet'
 import DesafioPlanetDescription from '@/components/segmentos/desafios/planets/[id]/components/desafio-planets-description'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Suspense } from 'react'
@@ -30,14 +29,11 @@ const PageDinamicaDesafioPlanet = ({
   return (
     <main className="container flex flex-col gap-8">
       <Suspense fallback={<Skeleton className="h-80 w-full" />}>
-        <ChallengerCompleted
+        <ChallengerCompletedPlanet
           id={id}
           page={page ?? '1'}
           challengerCompleted={challengerCompleted}
-          fetchCategory="planets"
-        >
-          <DesafioPlanets id={id} page={currentPage} />{' '}
-        </ChallengerCompleted>
+        />
       </Suspense>
       <Suspense fallback={<SkeletonDesafioPersonDescription />}>
         <DesafioPlanetDescription id={id} page={currentPage} />
