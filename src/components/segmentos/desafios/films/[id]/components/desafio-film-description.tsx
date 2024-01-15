407c4e5ac8c41971d044a1fb25518a1c66f28619
jhean <brizadaojhean@gmail.com>
2024-01-15 19:32:38 -0300

    :sparkles: feat: Component ListFilm

--- src/components/segmentos/desafios/films/[id]/components/desafio-film-description.tsx
+++ src/components/segmentos/desafios/films/[id]/components/desafio-film-description.tsx
@@ -1,25 +1,11 @@
 import { buttonVariants } from '@/components/ui/button'
-import SkeletonList from '@/components/ui/skeletons/skeleton-list'
-import {
-  TypographyH2,
-  TypographyList,
-  TypographyListItem,
-  TypographyP,
-  TypographySmall
-} from '@/components/ui/typography'
+import { TypographyH2 } from '@/components/ui/typography'
 import { TypeFetch, TypeFilms } from '@/types/Typesfetch'
 import { BASE_URL_API, fetchGet } from '@/utils/fetch'
 import { modifying_data_Films_improve_UI } from '@/utils/modiying_data_improve_ui'
 import { NavigationIcon } from 'lucide-react'
 import Link from 'next/link'
-import { Fragment, Suspense } from 'react'
-import DescriptionSubListPlanet from '../../../components/description-sub-list-planet'
-import DescriptionSubListSpecies from '../../../components/description-sub-list-species'
-import DescriptionSubListStarships from '../../../components/description-sub-list-starships'
-import DescriptionSubListVehicles from '../../../components/description-sub-list-vehicles'
-import SubList from '../../../components/section-species/sub-list'
-import ShorthandListItem from '../../../components/shorthand-list-item'
-import DescriptionSubListPerson from '../../../planets/[id]/components/description-subList-person'
+import ListFilm from '../../../components/list-film'
 
 type TypeDesafioFilmDescriptionProps = {
   id: string
@@ -57,94 +43,7 @@ const DesafioFilmDescription = async ({
           </Link>
         )}
       </div>
-      <TypographyList className="my-2 w-full max-w-3xl">
-        {dataFormated.map(
-          ({
-            created,
-            edited,
-            url,
-            episode_id,
-            opening_crawl,
-            director,
-            producer,
-            release_date,
-            species,
-            starships,
-            vehicles,
-            characters,
-            planets
-          }) => {
-            return (
-              <Fragment key={created + edited + url}>
-                <TypographyListItem>
-                  O número do episódio deste filme:
-                  {episode_id}
-                </TypographyListItem>
-                <ShorthandListItem
-                  title="O nome do diretor deste filme:"
-                  caracteristica={director}
-                />
-                <TypographyListItem>
-                  <TypographySmall>
-                    O(s) nome(s) do(s) produtor(es) deste filme:
-                  </TypographySmall>
-                  <SubList dataCaracteristicas={producer.split(',')} />
-                </TypographyListItem>
-                <ShorthandListItem
-                  title="O formato de data ISO 8601 de lançamento do filme no país do criador original:"
-                  caracteristica={release_date}
-                />
-                {species && species.length > 0 && (
-                  <Suspense fallback={<SkeletonList />}>
-                    <DescriptionSubListSpecies
-                      urls={species}
-                      title="Espécies que estão neste filme"
-                    />
-                  </Suspense>
-                )}
-                {starships && starships.length > 0 && (
-                  <Suspense fallback={<SkeletonList />}>
-                    <DescriptionSubListStarships
-                      urls={starships}
-                      title="Naves estelares que estão neste filme"
-                    />
-                  </Suspense>
-                )}
-                {vehicles && vehicles.length > 0 && (
-                  <Suspense fallback={<SkeletonList />}>
-                    <DescriptionSubListVehicles
-                      urls={vehicles}
-                      title="Veículos que estão neste filme:"
-                    />
-                  </Suspense>
-                )}
-                {characters && characters.length > 0 && (
-                  <Suspense fallback={<SkeletonList />}>
-                    <DescriptionSubListPerson
-                      urls={characters}
-                      title="Pessoas que estão neste filme:"
-                    />
-                  </Suspense>
-                )}
-                {planets && planets.length > 0 && (
-                  <Suspense fallback={<SkeletonList />}>
-                    <DescriptionSubListPlanet
-                      urls={planets}
-                      title="Planetários que estão neste filme:"
-                    />
-                  </Suspense>
-                )}
-                <TypographyListItem>
-                  <TypographySmall className="font-sans">
-                    Os parágrafos iniciais no início deste filme:
-                  </TypographySmall>
-                  <TypographyP className="text-sm">{opening_crawl}</TypographyP>
-                </TypographyListItem>
-              </Fragment>
-            )
-          }
-        )}
-      </TypographyList>
+      <ListFilm dataFilm={dataFormated} />
     </section>
   )
 }
