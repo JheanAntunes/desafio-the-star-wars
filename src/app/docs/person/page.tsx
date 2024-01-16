b9052b4f4d5fd4ad6b85684bd10bc61668694735
jhean <brizadaojhean@gmail.com>
2024-01-16 18:36:02 -0300

    :sparkles: feat: Component PagePersonDesafio: Representa a UI do segmento-rota person de docs

---
+++ src/app/docs/person/page.tsx
@@ -0,0 +1,33 @@
+import PersonCards from '@/components/segmentos/desafios/person/components/person-cards'
+import PersonPagination from '@/components/segmentos/desafios/person/components/person-pagination'
+import { SkeletonCards } from '@/components/ui/skeletons/skeleton-cards'
+import SkeletonPagination from '@/components/ui/skeletons/skeleton-pagination'
+import { TypographyH2 } from '@/components/ui/typography'
+import { Suspense } from 'react'
+
+type TypeParamsSearch = {
+  page?: string
+}
+function PagePersonDesafio({
+  searchParams
+}: {
+  searchParams: TypeParamsSearch
+}) {
+  const { page } = searchParams
+
+  return (
+    <main className="container my-8 flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <TypographyH2>Documentação de Pessoa</TypographyH2>
+      </div>
+      <Suspense fallback={<SkeletonCards />}>
+        <PersonCards page={page} segmento="docs" />
+      </Suspense>
+      <Suspense fallback={<SkeletonPagination />}>
+        <PersonPagination page={page} segmento="docs" />
+      </Suspense>
+    </main>
+  )
+}
+
+export default PagePersonDesafio
