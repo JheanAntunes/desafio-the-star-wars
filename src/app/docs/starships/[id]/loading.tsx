2677df8cf8032fcd5465c8ff15cf2e6bf68a7486
jhean <brizadaojhean@gmail.com>
2024-01-18 19:51:34 -0300

    :sparkles: feat: Component LoadingDinamicoStarships: Carregamento da UI da página dinamica de starships, Segmento: /docs/starships/[id]

---
+++ src/app/docs/starships/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingDinamico from '@/components/ui/skeletons/LoadingDinamico'
+
+const LoadingDinamicoStarships = () => {
+  return (
+    <>
+      <LoadingDinamico />
+    </>
+  )
+}
+
+export default LoadingDinamicoStarships
