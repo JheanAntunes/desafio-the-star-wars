3306ef3c2e91bbe721e0c52365ce34596f0e6048
jhean <brizadaojhean@gmail.com>
2024-01-17 23:19:26 -0300

    :sparkles: feat: Component PlanetDescription: adicionando uma nova prop segmento para reutilizar esse component

--- src/components/segmentos/components/planets-description.tsx
+++ src/components/segmentos/components/planets-description.tsx
@@ -1,3 +1,4 @@
+import { TypeSegmento } from '@/components/segmentos/types/segmento'
 import { hasNext } from '@/components/segmentos/utils/has-next'
 import { buttonVariants } from '@/components/ui/button'
 import { TypographyH2 } from '@/components/ui/typography'
@@ -6,17 +7,19 @@ import { BASE_URL_API, fetchGet } from '@/utils/fetch'
 import { modifying_data_Planet_improve_UI } from '@/utils/modiying_data_improve_ui'
 import { NavigationIcon } from 'lucide-react'
 import Link from 'next/link'
-import ListPlanet from '../../../../components/list-planet'
+import ListPlanet from './list-planet'
 
-type TypeDesafioPlanetDescriptionProps = {
+type TypePlanetDescriptionProps = {
   id: string
   page: string
+  segmento: TypeSegmento
 }
 
-const DesafioPlanetDescription = async ({
+const PlanetDescription = async ({
   id,
-  page
-}: TypeDesafioPlanetDescriptionProps) => {
+  page,
+  segmento
+}: TypePlanetDescriptionProps) => {
   const { results } = await fetchGet<TypeFetch<TypePlanet>>(
     `${BASE_URL_API}/planets/?page=${page}&format=json`
   )
@@ -32,13 +35,16 @@ const DesafioPlanetDescription = async ({
         <TypographyH2 className="order-2 md:order-1">Descrição</TypographyH2>
         {nextDesafio && (
           <Link
-            href={`/desafios/planets/${nextDesafio}?page=${page}`}
+            href={`/${segmento}/planets/${nextDesafio}?page=${page}`}
             className={buttonVariants({
               variant: 'outline',
               className: 'order-1 flex gap-1 font-sans md:order-2'
             })}
           >
-            Próximo Desafio <NavigationIcon width={20} height={20} />
+            {segmento === 'desafios'
+              ? 'Próximo Desafio'
+              : 'Próxima Documentação'}{' '}
+            <NavigationIcon width={20} height={20} />
           </Link>
         )}
       </div>
@@ -47,4 +53,4 @@ const DesafioPlanetDescription = async ({
   )
 }
 
-export default DesafioPlanetDescription
+export default PlanetDescription
