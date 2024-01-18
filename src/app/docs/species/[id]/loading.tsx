afbf35dd541e583aaa5b4e5f1db72e2498c83ae9
jhean <brizadaojhean@gmail.com>
2024-01-18 19:36:21 -0300

    :sparkles: feat: Component LoadingDinamicoSpecies: Carregamento da UI da página dinamica de species

---
+++ src/app/docs/species/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingDinamico from '@/components/ui/skeletons/LoadingDinamico'
+
+const LoadingDinamicoSpecies = () => {
+  return (
+    <>
+      <LoadingDinamico />
+    </>
+  )
+}
+
+export default LoadingDinamicoSpecies
