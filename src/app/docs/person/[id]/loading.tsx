1765e8693b910719651eab73c460e091f0c78470
jhean <brizadaojhean@gmail.com>
2024-01-17 23:17:55 -0300

    :sparkles: feat: Component LoadingPersonDocs: conteúdo transofrmado em component e utilizando para reutilizar em outras loadings

--- src/app/docs/person/[id]/loading.tsx
+++ src/app/docs/person/[id]/loading.tsx
@@ -1,28 +1,10 @@
-import { Skeleton } from '@/components/ui/skeleton'
-import SkeletonDescription from '@/components/ui/skeletons/skeleton-description'
-import SkeletonList from '@/components/ui/skeletons/skeleton-list'
+import LoadingDinamicoDocs from '@/components/ui/skeletons/LoadingDinamicoDocs'
 
 const LoadingPersonDocs = () => {
   return (
-    <main className="container my-8 flex flex-col gap-8">
-      <Skeleton className="mx-auto h-16 w-80 lg:w-full lg:max-w-xl" />
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col md:flex-row md:justify-between">
-          <Skeleton className="h-9 w-20" />
-          <Skeleton className="h-10 w-32" />
-        </div>
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonDescription />
-        <SkeletonList />
-        <SkeletonList />
-        <SkeletonList />
-      </div>
-    </main>
+    <>
+      <LoadingDinamicoDocs />
+    </>
   )
 }
 
