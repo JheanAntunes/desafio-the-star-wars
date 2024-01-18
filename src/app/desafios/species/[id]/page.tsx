6bd3d2507b8b60722f81c3f467f4c624b8d45f7f
jhean <brizadaojhean@gmail.com>
2024-01-18 19:26:54 -0300

    :sparkles: feat: Component PageDinamicaDesafioSpecies: Enviando a prop segmento para o component SpeciesDescription

--- src/app/desafios/species/[id]/page.tsx
+++ src/app/desafios/species/[id]/page.tsx
@@ -1,5 +1,5 @@
+import SpeciesDescription from '@/components/segmentos/components/species-description'
 import ChallengerCompletedSpecies from '@/components/segmentos/desafios/species/[id]/components/challenge-completed-species'
-import SpeciesDescription from '@/components/segmentos/desafios/species/[id]/components/species-description'
 import { Skeleton } from '@/components/ui/skeleton'
 import SkeletonDescription from '@/components/ui/skeletons/skeleton-description'
 import { Suspense } from 'react'
@@ -36,7 +36,7 @@ const PageDinamicaDesafioSpecies = ({
         />
       </Suspense>
       <Suspense fallback={<SkeletonDescription />}>
-        <SpeciesDescription id={id} page={currentPage} />
+        <SpeciesDescription id={id} page={currentPage} segmento="desafios" />
       </Suspense>
     </main>
   )
