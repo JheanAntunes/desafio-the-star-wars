7179e4614aed9b4fc4eeff6fac8e31c552022c43
jhean <brizadaojhean@gmail.com>
2024-01-20 23:44:12 -0300

    :nail_care: styles: images inversas de themes, consertado

--- src/components/segmentos/components/Image-darth-vader-theme.tsx
+++ src/components/segmentos/components/Image-darth-vader-theme.tsx
@@ -1,5 +1,5 @@
-import ImgDarthVaderLight from '@/components/assets/darth-vader-dark.svg'
-import ImgDarthVaderDark from '@/components/assets/darth-vader-light.svg'
+import ImgDarthVaderDark from '@/components/assets/darth-vader-dark.svg'
+import ImgDarthVaderLight from '@/components/assets/darth-vader-light.svg'
 import Image from 'next/image'
 const ImageDarthVaderTheme = () => {
   return (
